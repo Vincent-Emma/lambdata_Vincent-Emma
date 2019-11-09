01373c5de7de496d050aec6452a16470d0a1203c
Vincent Emma <vincentcemma@gmail.com>
2019-11-09 10:05:14 -0500

    create virtual env; add helper functions

---
+++ lambdata_Vincent_Emma/df_utils.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import pandas as pd
+
+ONES = pd.Series(np.ones(20))
+ZEROS = pd.Series(np.zeros(20))
+
+def isna(df):
+	print(df.isna().sum())
+
+
+def correlation_matrix(df):
+	print(df.corr())
+
+
+
